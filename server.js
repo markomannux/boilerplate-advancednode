3080874483669c07fdd4e702395067dd3959e513
Marco <marco.mannucci@hey.com>
2020-08-28 17:14:58 +0200

    added passport

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 "use strict";
 
 require('dotenv').config();
+const passport = require('passport');
+const session = require('express-session');
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
@@ -11,6 +13,13 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.route("/").get((req, res) => {
