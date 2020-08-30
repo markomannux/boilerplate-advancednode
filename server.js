0f1bc0fc85d23239eae1f7ded76012cd6fe81f10
Marco Mannucci <m.mannucci@kiwisoftware.it>
2020-08-30 18:19:25 +0200

    Added logout endpoint

--- server.js
+++ server.js
@@ -51,6 +51,18 @@ app.route('/profile').get(ensureAuthenticated, (req, res) => {
   res.render('pug/profile', {username:req.user.username});
 })
 
+app.route('/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/')
+})
+
+app.use((req, res, next) => {
+  res.status(404)
+  .type('text')
+  .send('Not Found')
+})
+
 mongo.connect(process.env.DATABASE, { useUnifiedTopology: true }, (err, client) => {
   if (err) {
     console.log('Database error: ' + err);
